4572eb9deecb55f5bb8b91ee2315df5bf0ae358f
gmalho <gaurav.malhotra@nike.com>
2019-02-01 23:33:42 +0100

    Swagger png

--- loan-approver-model.py
+++ loan-approver-model.py
@@ -2,46 +2,45 @@ import h2o
 h2o.init()
 
 print("Import approved and rejected loan requests...")
-loans = h2o.import_file(path = "data/loan.csv")
-loans["bad_loan"] = loans["bad_loan"].asfactor()
+loaded_loans_data = h2o.import_file(path ="data/loan.csv")
+loaded_loans_data["bad_loan"] = loaded_loans_data["bad_loan"].asfactor()
 
-rand  = loans.runif(seed = 1234567)
-train = loans[rand <= 0.8]
-valid = loans[rand > 0.8]
+rand  = loaded_loans_data.runif(seed = 1234567)
+train = loaded_loans_data[rand <= 0.8]
+valid = loaded_loans_data[rand > 0.8]
 
-myY = "bad_loan"
-myX = ["loan_amnt", "longest_credit_length", "revol_util", "emp_length",
+Y = "bad_loan"
+X = ["loan_amnt", "longest_credit_length", "revol_util", "emp_length",
        "home_ownership", "annual_inc", "purpose", "addr_state", "dti",
        "delinq_2yrs", "total_acc", "verification_status", "term"]
 
 from h2o.estimators.gbm import H2OGradientBoostingEstimator
-model = H2OGradientBoostingEstimator(score_each_iteration = True,
-                                     ntrees = 100,
-                                     max_depth = 5,
-                                     learn_rate = 0.05,
-                                     model_id = "AtrociousLoanModel")
-model.train(x = myX, y = myY, training_frame = train, validation_frame = valid)
-print(model)
+loans_model = H2OGradientBoostingEstimator(score_each_iteration = True,
+                                           ntrees = 100,
+                                           max_depth = 5,
+                                           learn_rate = 0.05,
+                                           model_id = "BadLoanModel")
+loans_model.train(x = X, y = Y, training_frame = train, validation_frame = valid)
+print(loans_model)
 
 # Download generated POJO for model
 import os
 if not os.path.exists("tmp"):
     os.makedirs("tmp")
-model.download_pojo(path = "tmp")
+loans_model.download_pojo(path ="tmp")
 
-myY = "int_rate"
-myX = ["loan_amnt", "longest_credit_length", "revol_util", "emp_length",
+Y = "int_rate"
+X = ["loan_amnt", "longest_credit_length", "revol_util", "emp_length",
        "home_ownership", "annual_inc", "purpose", "addr_state", "dti",
        "delinq_2yrs", "total_acc", "verification_status", "term"]
 
-model = H2OGradientBoostingEstimator(score_each_iteration = True,
-                                     ntrees = 100,
-                                     max_depth = 5,
-                                     learn_rate = 0.05,
-                                     model_id = "\"
-                                    a)
-model.train(x = myX, y = myY, training_frame = train, validation_frame = valid)
-print(model)
+loans_model = H2OGradientBoostingEstimator(score_each_iteration = True,
+                                           ntrees = 100,
+                                           max_depth = 5,
+                                           learn_rate = 0.05,
+                                           model_id = "InterestRateModel")
+loans_model.train(x = X, y = Y, training_frame = train, validation_frame = valid)
+print(loans_model)
 
 # Download generated POJO for model
-model.download_pojo(path = "tmp")
+loans_model.download_pojo(path ="tmp")
